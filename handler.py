8e788a8f86e580983f28c97ead2b287c43bba88d
1oid <1398231927@qq.com>
2025-06-09 11:23:40 +0800

    fix: /api/chat route dynamic settings

--- handler.py
+++ handler.py
@@ -7,20 +7,28 @@ def handler(event):
     """
     This is the handler function that will be called by RunPod serverless.
     """
-    job_input = event['input']
+    job_input = event.get('input', None)
+    job_route_path = event.get('path', '/api/generate')
+
+    if job_input is None:
+        return {
+            'status': 500,
+            'message': 'input is null'
+        }
+
 
     r = requests.post(
-        'http://127.0.0.1:11434/api/chat',
+        'http://127.0.0.1:11434' + job_route_path,
         headers={
             'Content-Type': 'application/json'
         },
-        data=json.dumps(job_input)
+        data=json.dumps(job_input, ensure_ascii=False)
     )
 
     return {
-        "message": "Hello, world!",
-        "event": json.dumps(event),
-        "resp": r.json()
+        "resp": r.json(),
+        "status": 200,
+        "message": "success"
     }
 
 
